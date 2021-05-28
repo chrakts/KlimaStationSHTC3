c105f00fc29c5b2a382b31bb62264519c1f1c1aa
Christof <chrak@cmja.de>
2021-05-28 21:36:47 +0200

    Erstausgabe

---
+++ KlimaStationSHTC3.cpp
@@ -0,0 +1,202 @@
+/*
+* LuefterSteuerung.cpp
+*
+* Created: 16.03.2017 13:03:01
+* Author : a16007
+*/
+
+#include "KlimaStationSHTC3.h"
+
+void setup()
+{
+  init_clock(SYSCLK,PLL,true,CLOCK_CALIBRATION);
+	PORTA_DIRSET = PIN2_bm | PIN3_bm | PIN4_bm;
+	PORTA_OUTSET = 0xff;
+
+	PORTB_DIRSET = 0xff;
+
+	PORTC_DIRSET = PIN1_bm;
+
+	PORTD_DIRSET = PIN0_bm | PIN4_bm | PIN5_bm | PIN7_bm;
+	PORTD_DIRCLR = PIN6_bm;
+	PORTD_OUTCLR = PIN4_bm | PIN5_bm;
+
+	PORTE_DIRSET = 0xff;
+  LEDROTSETUP;
+  LEDGRUENSETUP;
+  LEDBLAUSETUP;
+
+
+	uint8_t i;
+
+  TWI_MasterInit(&twiC_Master, &TWIC, TWI_MASTER_INTLVL_LO_gc, TWI_BAUDSETTING);
+
+	for(i=0;i<20;i++)
+	{
+		LEDGRUEN_TOGGLE;
+		_delay_ms(50);
+	}
+  LEDGRUEN_OFF;
+
+	PMIC_CTRL = PMIC_LOLVLEX_bm | PMIC_HILVLEN_bm | PMIC_MEDLVLEN_bm;
+	sei();
+
+	cnet.open(Serial::BAUD_57600,F_CPU);
+}
+
+int main(void)
+{
+uint8_t reportStarted = false;
+
+	setup();
+
+	cnet.broadcastUInt8((uint8_t) RST.STATUS,'S','0','R');
+
+	init_mytimer();
+
+	uint8_t sensorReady=SENSOR_READY;
+  _delay_ms(100);
+
+  humiSensor.begin(&twiC_Master);
+  humiSensor.setMode(SHTC3_NORMAL_T_FIRST);
+  cnet.broadcastUInt16(humiSensor.getID(),'S','I','D');
+
+	while (1)
+	{
+		cnetRec.comStateMachine();
+		cnetRec.doJob();
+
+		switch(statusSensoren)
+		{
+			case KLIMASENSOR:
+				sensorReady = doClima();
+			break;
+			case LASTSENSOR:
+				LEDROT_OFF;
+				sensorReady = doLastSensor();
+			break;
+		}
+		if (sensorReady==SENSOR_READY)
+		{
+			statusSensoren++;
+			if (statusSensoren>LASTSENSOR)
+			{
+				statusSensoren = KLIMASENSOR;
+				if(reportStarted==false)
+                {
+                    MyTimers[TIMER_REPORT].state = TM_START;
+                    reportStarted = true;
+                }
+			}
+		}
+		if( sendStatusReport )
+    {
+        char buffer[16];
+        sendStatusReport = false;
+        MyTimers[TIMER_REPORT].value = actReportBetweenSensors;
+        MyTimers[TIMER_REPORT].state = TM_START;
+        switch(statusReport)
+        {
+            case TEMPREPORT:
+                LEDGRUEN_ON;
+                sprintf(buffer,"%.1f",(double)fTemperatur);
+                cnet.sendStandard(buffer,BROADCAST,'C','1','t','F');
+            break;
+            case HUMIREPORT:
+                sprintf(buffer,"%.1f",(double)fHumidity);
+                cnet.sendStandard(buffer,BROADCAST,'C','1','h','F');
+            break;
+            case ABSHUMIREPORT:
+                sprintf(buffer,"%.1f",(double)fAbsHumitdity);
+                cnet.sendStandard(buffer,BROADCAST,'C','1','a','F');
+            break;
+            case DEWPOINTREPORT:
+                sprintf(buffer,"%.1f",(double)fDewPoint);
+                cnet.sendStandard(buffer,BROADCAST,'C','1','d','F');
+            break;
+            case LASTREPORT:
+                LEDGRUEN_OFF;
+                MyTimers[TIMER_REPORT].value = actReportBetweenBlocks;
+                MyTimers[TIMER_REPORT].state = TM_START;
+            break;
+        }
+    }
+
+	}
+}
+
+uint8_t doLastSensor()
+{
+	switch( statusLastSensor )
+	{
+		case NOTHING_LAST_TODO:
+			MyTimers[TIMER_TEMPERATURE].value = actWaitAfterLastSensor;
+			MyTimers[TIMER_TEMPERATURE].state = TM_START;
+			statusLastSensor = WAIT_LAST;
+		break;
+		case READY_LAST:
+			statusLastSensor = NOTHING_LAST_TODO;
+		break;
+	}
+	return statusLastSensor;
+}
+
+uint8_t doClima()
+{
+bool noError;
+
+	switch(statusKlima)
+	{
+		case NOTHING_CLIMA_TODO:
+			statusKlima = WAKEUP;
+		break;
+		case WAKEUP:
+		  humiSensor.wakeup();
+		  statusKlima = WAIT_WAKEUP;
+      MyTimers[TIMER_TEMPERATURE].value = 2; //22
+      MyTimers[TIMER_TEMPERATURE].state = TM_START;
+    break;
+
+		case START_CONVERSION: //
+			LEDROT_ON;
+
+			noError=humiSensor.startMeasure();
+			if (noError==true)
+			{
+				statusKlima = WAIT_CONVERSION;
+				MyTimers[TIMER_TEMPERATURE].value = 2; //22
+				MyTimers[TIMER_TEMPERATURE].state = TM_START;
+			}
+			else
+				statusKlima = NOTHING_CLIMA_TODO;
+		break;
+
+		case READ_CONVERSION:  // Durchlaufzeit ca.
+			noError = humiSensor.readResults();
+			if (noError==true)
+			{
+				statusKlima = WAIT_READ;
+				MyTimers[TIMER_TEMPERATURE].value = 2; //22
+				MyTimers[TIMER_TEMPERATURE].state = TM_START;
+			}
+			else
+				statusKlima = NOTHING_CLIMA_TODO;
+		break;
+		case CALC_CONVERSION0:  // Durchlaufzeit ca.
+		  humiSensor.getResults(fTemperatur,fHumidity);
+		  humiSensor.sleep();
+			statusKlima = CALC_CONVERSION1;
+		break;
+		case CALC_CONVERSION1:  // Durchlaufzeit ca.
+		  fAbsHumitdity =  humiSensor.calcAbsHumi(fTemperatur,fHumidity);
+			statusKlima = CALC_CONVERSION2;
+		break;
+		case CALC_CONVERSION2:  // Durchlaufzeit ca.
+		  fDewPoint =  humiSensor.calcDewPoint(fTemperatur,fHumidity);
+			statusKlima = NOTHING_CLIMA_TODO;
+		break;
+	}
+	return(statusKlima);
+}
+
+
